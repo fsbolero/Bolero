9058ad166931d56fb1f270a5ff6318ee4f4ebf30
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-09-26 16:40:12 +0200

    First version of diffing renderer

--- Html.fs
+++ Html.fs
@@ -1,69 +1,38 @@
 module rec MiniBlazor.Html
 
+open System.Collections.Generic
 open System.Text
 open System.Net
 open Microsoft.JSInterop.Internal
 
-type Attr =
-    {
-        Name: string
-        Value: string
-    }
-
-    interface ICustomArgSerializer with
-        member this.ToJsonPrimitive() =
-            box (StringBuilder().Append(this).ToString())
-
-module Attr =
-
-    let Make name value =
-        { Name = name; Value = value }
-
-type Node =
-    | Elt of name: string * attrs: list<Attr> * children: list<Node>
+type Node<'Message> =
+    | Elt of name: string * attrs: IDictionary<string, string> * events: IDictionary<string, obj -> 'Message> * children: list<Node<'Message>>
     | Text of text: string
 
-    override this.ToString() = StringBuilder().Append(this).ToString()
+[<Struct>]
+type Attr<'Message> =
+    | PlainAttr of aname: string * avalue: string
+    | Handler of ename: string * ehandler: (obj -> 'Message)
 
-    interface ICustomArgSerializer with
-        member this.ToJsonPrimitive() =
-            box (StringBuilder().Append(this).ToString())
-
-type StringBuilder with
-
-    member this.Append(attr: Attr) =
-        this.Append(' ')
-            .Append(attr.Name)
-            .Append("=\"")
-            .Append(WebUtility.HtmlEncode(attr.Value))
-            .Append('"')
-
-    member this.Append(attrs: list<Attr>) =
-        attrs |> List.iter (this.Append >> ignore)
-        this
-
-    member this.Append(nodes: list<Node>) =
-        nodes |> List.iter (this.Append >> ignore)
-        this
-
-    member this.Append(node: Node) =
-        match node with
-        | Elt(name, attrs, children) ->
-            this.Append('<')
-                .Append(name)
-                .Append(attrs)
-                .Append('>')
-                .Append(children)
-                .Append("</")
-                .Append(name)
-                .Append('>')
-        | Text text ->
-            this.Append(WebUtility.HtmlEncode(text))
+let Element name attrsAndEvents children =
+    let attrs = Dictionary<string, string>()
+    let events = Dictionary<string, obj -> 'Message>()
+    for a in attrsAndEvents do
+        match a with
+        | PlainAttr(name, value) -> attrs.[name] <- value
+        | Handler(name, handler) -> events.[name] <- handler
+    Elt(name, attrs, events, children)
 
 let text str = Text str
-
-let div attrs children = Elt ("div", attrs, children)
-let b attrs children = Elt ("b", attrs, children)
-let i attrs children = Elt ("i", attrs, children)
-
-let style value = Attr.Make "style" value
+let div attrs children = Element "div" attrs children
+let input attrs = Element "input" attrs []
+let b attrs children = Element "b" attrs children
+let i attrs children = Element "i" attrs children
+let (=>) name value = PlainAttr(name, value)
+let value x = "value" => x
+let type_ x = "type" => x
+let onLazy event handler = Handler(event, handler)
+let on event message = Handler(event, fun _ -> message)
+let onChange message = onLazy "change" (fun args -> message (args :?> string))
+let onInput message = onLazy "input" (fun args -> message (args :?> string))
+let onClick message = on "click" message
