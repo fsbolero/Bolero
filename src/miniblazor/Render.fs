5f74cc0fb0b0630a0eef54378f8dd3671a612d75
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-09-27 10:37:26 +0200

    Revert premature optimization for json

--- src/miniblazor/Render.fs
+++ src/miniblazor/Render.fs
@@ -5,33 +5,47 @@ open Microsoft.JSInterop
 open Microsoft.JSInterop.Internal
 open MiniBlazor.Html
 
-// This is an obj because we want to use different concrete types to optimize JSON serialization.
-type RenderedNode = obj
+type RenderedNode =
+    | RText of text: string
+    | RElt of name: string * attrs: IDictionary<string, string> * events: IDictionary<string, obj> * children: RenderedNode[]
 
-type RNode =
+    interface ICustomArgSerializer with
+        member this.ToJsonPrimitive() =
+            match this with
+            | RText text -> box text
+            | RElt(name, attrs, events, children) ->
+                box { n = name; a = attrs; e = events; c = children }
+
+and DiffResult =
+    | Skip
+    | Replace of RenderedNode
+    | Insert of RenderedNode
+    | InPlace of attrs: IDictionary<string, string> * events: IDictionary<string, obj> * children: DiffResult[]
+
+    interface ICustomArgSerializer with
+        member this.ToJsonPrimitive() =
+            match this with
+            | Skip -> box "s"
+            | Replace node -> box { r = node }
+            | Insert node -> box { i = node }
+            | InPlace(attrs, events, children) ->
+                box { a = attrs; e = events; c = children }
+
+and RNode =
     { n: string
       a: IDictionary<string, string>
       e: IDictionary<string, obj>
       c: RenderedNode[] }
 
-let rnode name attrs events children : RenderedNode =
-    box { n = name; a = attrs; e = events; c = children }
-
-let rtext (txt: string) : RenderedNode =
-    box txt
-
-// This is an obj because we want to use different concrete types to optimize JSON serialization.
-type DiffResult = obj
-
-type InPlaceDiff =
+and InPlaceDiff =
     { a: IDictionary<string, string>
       e: IDictionary<string, obj>
       c: DiffResult[] }
 
-type ReplaceDiff =
+and ReplaceDiff =
     { r: RenderedNode }
 
-type InsertDiff =
+and InsertDiff =
     { i: RenderedNode }
 
 type RenderedEvent =
@@ -41,13 +55,13 @@ type RenderedEvent =
     member this.Handle(args) = this.handler(args)
 
 let rec toRenderedNode wrapEvent = function
-    | Text text -> rtext text
+    | Text text -> RText text
     | Elt(name, attrs, events, children) ->
         let revents = Dictionary<string, obj>()
         for KeyValue(name, handler) in events do
             let handler : RenderedEvent = { handler = wrapEvent handler }
             revents.Add(name, new DotNetObjectRef(handler))
-        rnode name attrs revents (toRenderedNodes wrapEvent children)
+        RElt(name, attrs, revents, toRenderedNodes wrapEvent children)
     | Empty | Concat _ -> failwith "Should not have composite nodes at render time"
 
 and toRenderedNodes wrapEvent nodes =
@@ -62,30 +76,22 @@ and toRenderedNodes wrapEvent nodes =
     go nodes
     acc.ToArray()
 
-let skip = box "s"
-
-let isSkip (x: DiffResult) =
-    match x with
-    | :? string as s when s = "s" -> true
-    | _ -> false
+let isSkip = function Skip -> true | _ -> false
 
 let replace wrapEvent node =
     let rendered = toRenderedNode wrapEvent node
-    box { r = rendered }, rendered
+    Replace rendered, rendered
 
 let insert wrapEvent node =
     let rendered = toRenderedNode wrapEvent node
-    box { i = rendered }, rendered
-
-let inPlace attrs events children =
-    box { a = attrs; e = events; c = children }
+    Insert rendered, rendered
 
 /// Diff between the currently rendered node and the new content.
 /// Assumes that `after` is a single node, ie either Text or Elt.
 let rec private diff wrapEvent (before: RenderedNode) (after: Node<'Message>) : DiffResult * RenderedNode =
     match before, after with
-    | :? RNode as b, Elt (aname, aattrs, aevents, achildren) ->
-        if aname = b.n then
+    | RElt(bname, battrs, bevents, bchildren), Elt (aname, aattrs, aevents, achildren) ->
+        if aname = bname then
         // Same node type: apply in-place changes.
             let attrDiff = Dictionary<string, string>() // Attribute diff: null to remove, value to set.
             let eventDiff = Dictionary<string, obj>() // Event diff: null to remove, value to set.
@@ -93,20 +99,20 @@ let rec private diff wrapEvent (before: RenderedNode) (after: Node<'Message>) : 
             // 1. Diff attributes
             //   1.1. Add attributes that weren't there yet or have changed.
             for KeyValue(k, av) in aattrs do
-                match b.a.TryGetValue(k) with
+                match battrs.TryGetValue(k) with
                 | true, bv ->
                     if av <> bv then attrDiff.Add(k, av)
                 | false, _ ->
                     attrDiff.Add(k, av)
             //   1.2. Remove attributes that won't be there anymore.
-            for KeyValue(k, _) in b.a do
+            for KeyValue(k, _) in battrs do
                 if not (aattrs.ContainsKey k) then
                     attrDiff.Add(k, null)
             // 2. Diff events
             //   2.1. Add events that weren't there yet.
             for KeyValue(k, av) in aevents do
                 let av =
-                    match b.e.TryGetValue(k) with
+                    match bevents.TryGetValue(k) with
                     | true, bv ->
                         // If the event already existed, we can just set the callback locally.
                         let bv = bv :?> DotNetObjectRef
@@ -118,31 +124,31 @@ let rec private diff wrapEvent (before: RenderedNode) (after: Node<'Message>) : 
                         av
                 revents.Add(k, av)
             //   2.2. Remove events that aren't there anymore.
-            for KeyValue(k, v) in b.e do
-                if not (b.e.ContainsKey k) then
+            for KeyValue(k, v) in bevents do
+                if not (bevents.ContainsKey k) then
                     let v = v :?> DotNetObjectRef
                     v.Dispose()
                     eventDiff.Add(k, null)
             // 3. Diff children.
-            let childDiff, newChildren = diffSiblings wrapEvent b.c achildren
+            let childDiff, newChildren = diffSiblings wrapEvent bchildren achildren
             if  attrDiff.Count = 0 &&
                 eventDiff.Count = 0 &&
-                childDiff |> Array.forall (function :? string as s when s = "s" -> true | _ -> false)
+                childDiff |> Array.forall isSkip
             // If there are no changes to attributes, events, nor children, then we can skip.
             then
-                skip, before
+                Skip, before
             else
             // Otherwise, apply in-place changes.
-                let diff = inPlace attrDiff eventDiff childDiff
-                let newTree = rnode aname aattrs revents newChildren
+                let diff = InPlace(attrDiff, eventDiff, childDiff)
+                let newTree = RElt(aname, aattrs, revents, newChildren)
                 diff, newTree
         else
         // Different type nodes: completely replace.
             replace wrapEvent after
-    | :? string as btext, Text atext ->
+    | RText btext, Text atext ->
         // Both text nodes: replace iff the content is different.
         if atext = btext then
-            skip, before
+            Skip, before
         else
             replace wrapEvent after
     | _ ->
@@ -159,7 +165,7 @@ and diffSiblings wrapEvent (before: RenderedNode[]) (after: list<Node<'Message>>
         | Empty :: arest -> go i arest
         | Concat a :: arest -> go (go i a) arest
         | a :: arest ->
-            if i = before.Length then
+            if i >= before.Length then
                 insert wrapEvent a
             else
                 diff wrapEvent before.[i] a
