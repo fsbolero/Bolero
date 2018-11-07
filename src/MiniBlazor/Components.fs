3ce605267658d0eec49a45d27c2069c17d05472b
Loic Denuziere <loic@denuziere.net>
2018-11-07 12:07:50 +0100

    Add virtual ElmishComponent.ShouldRender(oldModel, newModel)

--- src/MiniBlazor/Components.fs
+++ src/MiniBlazor/Components.fs
@@ -41,8 +41,14 @@ type ElmishComponent<'model, 'msg>() =
     /// The Elmish view function.
     abstract View : 'model -> Dispatch<'msg> -> Node
 
+    /// Compare the old model with the new to decide whether this component
+    /// needs to be re-rendered.
+    abstract ShouldRender : oldModel: 'model * newModel: 'model -> bool
+    default this.ShouldRender(oldModel, newModel) =
+        not <| obj.ReferenceEquals(oldModel, newModel)
+
     override this.ShouldRender() =
-       not <| obj.ReferenceEquals(oldModel, this.Model)
+       this.ShouldRender(oldModel, this.Model)
 
     override this.Render() =
         oldModel <- this.Model
