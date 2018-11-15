55bc42b6da488eb945bf7da810535743896262e3
Loic Denuziere <loic@denuziere.net>
2018-11-15 16:25:15 +0100

    Add renderer debug output when Bolero is built in debug mode

--- src/Bolero/Render.fs
+++ src/Bolero/Render.fs
@@ -3,8 +3,82 @@ module Bolero.Render
 open System
 open System.Collections.Generic
 open FSharp.Reflection
+
+#if !DEBUG_RENDERER
 open Microsoft.AspNetCore.Blazor
 open Microsoft.AspNetCore.Blazor.RenderTree
+#else
+open System.IO
+
+type BlazorTreeBuilder = Microsoft.AspNetCore.Blazor.RenderTree.RenderTreeBuilder
+type BlazorFragment = Microsoft.AspNetCore.Blazor.RenderFragment
+
+type RenderTreeBuilder(b: BlazorTreeBuilder, indent: int, out: TextWriter) =
+    let mutable indent = indent
+
+    let write fmt =
+            Printf.kprintf (fun s ->
+                out.Write(Array.create (indent * 2) ' ')
+                out.Write(s)
+                out.Flush()
+            ) fmt
+
+    let writen fmt =
+            Printf.kprintf (fun s ->
+                out.Write(Array.create (indent * 2) ' ')
+                out.WriteLine(s)
+                out.Flush()
+            ) fmt
+
+    member this.AddContent(sequence: int, text: string) =
+        if not (isNull b) then b.AddContent(sequence, text)
+        writen "Text %i %s" sequence text
+
+    member this.AddContent(sequence: int, frag: RenderFragment) =
+        if not (isNull b) then b.AddContent(sequence, frag.Frag)
+        write "Frag %i\n%s" sequence (frag.Show(indent + 1))
+
+    member this.AddMarkupContent(sequence: int, markup: string) =
+        if not (isNull b) then b.AddMarkupContent(sequence, markup)
+        writen "Markup %i %s" sequence markup
+
+    member this.OpenElement(sequence: int, name: string) =
+        if not (isNull b) then b.OpenElement(sequence, name)
+        writen "OpenElt %i %s" sequence name
+        indent <- indent + 1
+
+    member this.CloseElement() =
+        indent <- indent - 1
+        if not (isNull b) then b.CloseElement()
+        writen "CloseElt"
+
+    member this.OpenComponent(sequence: int, ty: Type) =
+        if not (isNull b) then b.OpenComponent(sequence, ty)
+        writen "OpenComp %i %s" sequence ty.FullName
+        indent <- indent + 1
+
+    member this.CloseComponent() =
+        indent <- indent - 1
+        if not (isNull b) then b.CloseComponent()
+        writen "CloseComp"
+
+    member this.AddAttribute(sequence: int, name: string, value: obj) =
+        match value with
+        | :? RenderFragment as f ->
+            if not (isNull b) then b.AddAttribute(sequence, name, f.Frag)
+            writen "Attr %i %s\n%s" sequence name (f.Show(indent + 1))
+        | value ->
+            if not (isNull b) then b.AddAttribute(sequence, name, value)
+            writen "Attr %i %s %A" sequence name value
+
+and RenderFragment(f: RenderTreeBuilder -> unit) =
+    member this.Frag : BlazorFragment =
+        BlazorFragment(fun b -> f(RenderTreeBuilder(b, 0, TextWriter.Null)))
+    member this.Show(indent) =
+        use w = new StringWriter()
+        f(RenderTreeBuilder(null, indent, w))
+        w.ToString()
+#endif
 
 /// Render `node` into `builder` at `sequence` number.
 let rec renderNode (builder: RenderTreeBuilder) (matchCache: Type -> int * (obj -> int)) sequence node =
@@ -68,5 +142,8 @@ let RenderNode builder (matchCache: Dictionary<Type, _>) node =
             let v = (caseCount, r)
             matchCache.[ty] <- v
             v
+#if DEBUG_RENDERER
+    let builder = RenderTreeBuilder(builder, 0, stdout)
+#endif
     renderNode builder getMatchParams 0 node
     |> ignore
