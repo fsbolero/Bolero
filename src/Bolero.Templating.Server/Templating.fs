eb7187fa52ce2a53f27325ee4b3337d23c33cc95
Loic Denuziere <loic@denuziere.net>
2019-02-12 14:24:58 +0100

    Move hot reload functionality to separate nuget Bolero.HotReload

--- src/Bolero.Templating.Server/Templating.fs
+++ src/Bolero.Templating.Server/Templating.fs
@@ -152,7 +152,7 @@ type ServerTemplatingExtensions =
 
     [<Extension>]
     static member AddHotReload(this: IServiceCollection, ?templateDir: string) : IServiceCollection =
-        this.AddSignalRCore().AddJsonProtocol() |> ignore
+        this.AddSignalR().AddJsonProtocol() |> ignore
         this.AddSingleton({ dir = templateDir })
             .AddSingleton<Watcher>()
             .AddTransient<IClient, Client>()
