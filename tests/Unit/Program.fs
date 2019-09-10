994c545986b9930cd421624736f94a6497a8b5f4
Loic Denuziere <loic@denuziere.net>
2019-09-10 23:04:27 +0200

    Add entrypoint to Unit to run the test app manually

---
+++ tests/Unit/Program.fs
@@ -0,0 +1,33 @@
+// $begin{copyright}
+//
+// This file is part of Bolero
+//
+// Copyright (c) 2018 IntelliFactory and contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// $end{copyright}
+
+// Run the unit test app to test things manually
+module Bolero.Tests.Web.Program
+
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Hosting
+
+[<EntryPoint>]
+let Main args =
+    WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .Build()
+        .Run()
+    0
