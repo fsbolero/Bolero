f80061108e3554063cb299c231c37c440798c832
Loic Denuziere <loic@denuziere.net>
2020-11-01 17:20:47 +0100

    Add dummy implems of Blazor to keep the type provider netstandard2.0

---
+++ src/tpdummy/Microsoft.AspNetCore.Components.Web/Library.fs
@@ -0,0 +1,10 @@
+namespace Microsoft.AspNetCore.Components.Web
+
+type ClipboardEventArgs = class end
+type DragEventArgs = class end
+type FocusEventArgs = class end
+type KeyboardEventArgs = class end
+type MouseEventArgs = class end
+type PointerEventArgs = class end
+type ProgressEventArgs = class end
+type TouchEventArgs = class end
---
+++ src/tpdummy/Microsoft.AspNetCore.Components/Library.fs
@@ -0,0 +1,9 @@
+namespace Microsoft.AspNetCore.Components
+
+type ChangeEventArgs = member _.Value = obj()
+type RenderFragment = class end
+type IComponent = interface end
+
+namespace Microsoft.AspNetCore.Components.Rendering
+
+type RenderTreeBuilder = class end
