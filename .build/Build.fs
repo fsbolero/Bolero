dfd275c3736784fd87506f140b47d50f09503421
Loïc Denuzière <ldenuziere@d-edge.com>
2026-07-31 17:57:59 +0200

    refactor: put common test dependencies in paket group tests-common

--- .build/Build.fs
+++ .build/Build.fs
@@ -243,9 +243,7 @@ Target.create "update-chromedriver" (fun _ ->
             let ver = System.Diagnostics.FileVersionInfo.GetVersionInfo(f)
             Some $"{ver.FileMajorPart}.{ver.FileMinorPart}"
     |> Option.iter (fun v ->
-        dotnet "paket" ["update"; "Selenium.WebDriver.ChromeDriver"; "-V"; $"~> {v}"; "-g"; "tests-net8"]
-        dotnet "paket" ["update"; "Selenium.WebDriver.ChromeDriver"; "-V"; $"~> {v}"; "-g"; "tests-net9"]
-        dotnet "paket" ["update"; "Selenium.WebDriver.ChromeDriver"; "-V"; $"~> {v}"; "-g"; "tests-net10"])
+        dotnet "paket" ["update"; "Selenium.WebDriver.ChromeDriver"; "-V"; $"~> {v}"; "-g"; "tests-common"])
 )
 
 Target.description "Build, test and pack"
