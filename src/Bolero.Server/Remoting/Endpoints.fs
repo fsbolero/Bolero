6f4cfd38be540e279d47ab9be93c8acde3d12609
Loic Denuziere <loic@denuziere.net>
2026-08-09 18:32:38 +0200

    feat: Add IDisableCookieRedirectMetadata to remote endpoints

--- src/Bolero.Server/Remoting/Endpoints.fs
+++ src/Bolero.Server/Remoting/Endpoints.fs
@@ -69,6 +69,17 @@ type internal RemoteMethodInfo(method: IRemoteMethodMetadata) =
     override this.ReflectedType = null
 
 type internal RemotingServiceEndpointBuilder(service: RemotingService, buildEndpoint: Action<IRemoteMethodMetadata, IEndpointConventionBuilder>) =
+    static let httpMethodMetadata =
+        {
+            new IHttpMethodMetadata with
+                member _.AcceptCorsPreflight = true
+                member _.HttpMethods = ["POST"]
+        }
+
+#if NET10_0_OR_GREATER
+    static let disableCookieRedirectMetadata =
+       { new IDisableCookieRedirectMetadata }
+#endif
 
     let endpoints =
         service.Methods
@@ -76,20 +87,24 @@ type internal RemotingServiceEndpointBuilder(service: RemotingService, buildEndp
             let path = RoutePatternFactory.Parse $"{service.BasePath}/{methodName}"
             let endpoint = RouteEndpointBuilder(method.Handler, path, 0)
             endpoint.DisplayName <- $"Remote method {method.Name} on service {method.Service.Type.Name}"
-            ([ { new IAcceptsMetadata with
+            endpoint.Metadata.Add({
+                new IAcceptsMetadata with
                    member _.ContentTypes = ["application/json"]
                    member _.RequestType = method.ArgumentType
-                   member _.IsOptional = false }
-               { new IProducesResponseTypeMetadata with
+                   member _.IsOptional = false
+            })
+            endpoint.Metadata.Add({
+                new IProducesResponseTypeMetadata with
                    member _.ContentTypes = ["application/json"]
                    member _.StatusCode = 200
-                   member _.Type = method.ReturnType }
-               { new IHttpMethodMetadata with
-                   member _.AcceptCorsPreflight = true
-                   member _.HttpMethods = ["POST"] }
-               RemoteMethodInfo(method)
-               method ] : obj list)
-            |> List.iter endpoint.Metadata.Add
+                   member _.Type = method.ReturnType
+            })
+            endpoint.Metadata.Add(httpMethodMetadata)
+#if NET10_0_OR_GREATER
+            endpoint.Metadata.Add(disableCookieRedirectMetadata)
+#endif
+            endpoint.Metadata.Add(RemoteMethodInfo(method))
+            endpoint.Metadata.Add(method)
 
             match method.Function with
             | :? IAuthorizedMethodHandler as handler ->
